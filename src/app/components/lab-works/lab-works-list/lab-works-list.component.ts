e5e6e1a4c4a55f35139897397cca091a7de0f3e2
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-08 10:03:52 +0530

    Add pagination to lab works

--- src/app/components/lab-works/lab-works-list/lab-works-list.component.ts
+++ src/app/components/lab-works/lab-works-list/lab-works-list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewChild } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { PaginationComponent } from '../../shared/pagination/pagination.component';
 
 import { LabWork } from "../../../models/lab-work";
 
@@ -15,19 +20,29 @@ export class LabWorksListComponent implements OnInit {
   labWorks: LabWork[] = [];
   groupSlug: string;
   isLoadingLabWorks: boolean;
+  @ViewChild(PaginationComponent)
+  pagination: PaginationComponent;
 
   constructor(private authService: AuthService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      let page = params.page ? params.page: 1;
+      this.loadLabWorks(page);
+    });
+  }
+
+  loadLabWorks(page) {
     this.activatedRoute.parent.params.subscribe((params) => {
       this.groupSlug = params.slug;
     });
     this.activatedRoute.params.subscribe((params) => {
       let groupSlug = params.slug;
       this.isLoadingLabWorks = true;
-      this.authService.get(`groups/${groupSlug}/lab-works`, (response) => {
+      this.authService.get(`groups/${groupSlug}/lab-works?page=${page}`, (response) => {
         if(!response.error) {
-          this.labWorks = response.msg;
+          this.pagination.paginate(page, response.msg.limit, response.msg.total);
+          this.labWorks = response.msg.docs;
         }
         this.isLoadingLabWorks = false;
       });
