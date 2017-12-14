945572ec9af080126443a8277e71ddcaa5d13ac9
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-14 11:53:27 +0530

    Add ability to edit existing user details

--- src/app/components/users/users-form/users-form.component.ts
+++ src/app/components/users/users-form/users-form.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { 
+  FormGroup,
+  FormControl, 
+  Validators } from '@angular/forms';
+
+import { AuthService } from '../../../services/auth/auth.service';
+import { FlashMessageService } from '../../../services/flash-message/flash-message.service';
+import { ModalService } from '../../../services/modal/modal.service';
+
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'ic-users-form',
@@ -7,9 +18,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersFormComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  isEditForm: boolean = false;
+  isFormLoading: boolean = false;
+  usersForm: FormGroup;
+  user: User;
+  btnSubmitText = 'Submit';
+
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute, private router: Router, private flashMessageService: FlashMessageService, private modalService: ModalService) { }
 
   ngOnInit() {
+    this.user = new User();
+    this.usersForm = this.initUsersForm(this.user);
+
+    this.activatedRoute.params.subscribe((params) => {
+      this.username = params.username;
+      // Check if the form is for editing or for creation
+      if(this.username) {
+        this.isEditForm = true;
+        this.isFormLoading = true;
+        this.authService.get(`users/${this.username}`, (response) => {
+          if(!response.error) {
+            this.user = response.msg;
+            this.usersForm = this.initUsersForm(this.user);
+          }
+          this.isFormLoading = false;
+        }, false);
+      }
+      else {
+
+      }
+    });
+  }
+
+  onSubmitClick() {
+    let user = new User();
+
+    user.firstName = this.firstName.value;
+    user.lastName = this.lastName.value;
+    user.email = this.email.value;
+    user.gender = this.gender.value;
+    this.setFormProcessingStatus(true);
+    this.authService.put(`users/${this.username}`, user, (response) => {
+      if(!response.error) {
+        this.flashMessageService.addFlashMessage(['The user was updated!']);
+        this.router.navigate(['/users']);
+      }
+      else {
+        this.modalService.showModal('OOPS!', 'The user could not be updated');
+      }
+      this.setFormProcessingStatus(false);
+    }, false);
+  } 
+  private initUsersForm(user: User): FormGroup {
+    return new FormGroup({
+      'firstName': new FormControl(user.firstName, Validators.required),
+      'lastName': new FormControl(user.lastName),
+      'email': new FormControl(user.email, Validators.required),
+      'gender': new FormControl(user.gender, Validators.required)
+    });
+  }
+  private setFormProcessingStatus(status) {
+    this.isFormLoading = status;
+    if(status) {
+      this.btnSubmitText = 'Saving...';
+    }
+    else {
+      this.btnSubmitText = 'Submit';
+    }
+  }
+  get firstName() {
+    return this.usersForm.get('firstName');
+  }
+  get lastName() {
+    return this.usersForm.get('lastName');
+  }
+  get email() {
+    return this.usersForm.get('email');
+  }
+  get gender() {
+    return this.usersForm.get('gender');
   }
 
 }
