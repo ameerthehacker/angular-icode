370a642c2a8ef65e363fc5341d0d0bf600b3d149
ameerthehacker <ameerjhanprof@gmail.com>
2018-02-24 00:55:12 +0530

    Add firebase deployment :fire:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backend: {
-    server: "https://ec2-18-222-19-239.us-east-2.compute.amazonaws.com:3000"
+    server: "http://ec2-18-222-19-239.us-east-2.compute.amazonaws.com:3000"
   }
 };
